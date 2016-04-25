15801b17c772721dfe93a6eac8850901ae0e2cc7
Jason Hinchcliffe <hinchy@gmail.com>
2016-04-25 02:00:31 +0000

    added pages/maptest. Now provides links and pins in gmaps for each provider

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,6 +3,15 @@ class PagesController < ApplicationController
   end
   
   def maptest
-    @providerprofile = Providerprofile.last
+    @providerprofiles = Providerprofile.all
+    @pp = Providerprofile.all
+    @hash = Gmaps4rails.build_markers(@providerprofiles) do |providerprofile, marker|
+      if providerprofile.latitude.present? || providerprofile.longitude.present?
+        marker.lat providerprofile.latitude
+        marker.lng providerprofile.longitude
+        marker.json({:id => providerprofile.id })
+        marker.infowindow render_to_string(:partial => "/providerprofiles/infowindow", :locals => { :object => providerprofile})
+      end
+    end
   end
 end
